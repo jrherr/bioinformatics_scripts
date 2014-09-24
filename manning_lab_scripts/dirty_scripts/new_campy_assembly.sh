dabea1f3649ca752884295caff307ac1e2b7d99f
Josh Herr <joshua.r.herr@gmail.com>
2014-09-24 12:02:53 -0400

    update scripts

---
+++ manning_lab_scripts/dirty_scripts/new_campy_assembly.sh
@@ -0,0 +1,179 @@
+#! /bin/bash
+
+spades.py -1 TW16361/TW16361_forward.fastq -2 TW16361/TW16361_reverse.fastq --careful -t 7 -o ./TW16361/assembly
+
+spades.py -1 TW16373/TW16373_forward.fastq -2 TW16373/TW16373_reverse.fastq --careful -t 7 -o ./TW16373/assembly
+
+spades.py -1 TW16374/TW16374_forward.fastq -2 TW16374/TW16374_reverse.fastq --careful -t 7 -o ./TW16374/assembly
+
+spades.py -1 TW16398/TW16398_forward.fastq -2 TW16398/TW16398_reverse.fastq --careful -t 7 -o ./TW16398/assembly
+
+spades.py -1 TW16399/TW16399_forward.fastq -2 TW16399/TW16399_reverse.fastq --careful -t 7 -o ./TW16399/assembly
+
+spades.py -1 TW16400/TW16400_forward.fastq -2 TW16400/TW16400_reverse.fastq --careful -t 7 -o ./TW16400/assembly
+
+spades.py -1 TW16402/TW16402_forward.fastq -2 TW16402/TW16402_reverse.fastq --careful -t 7 -o ./TW16402/assembly
+
+spades.py -1 TW16409/TW16409_forward.fastq -2 TW16409/TW16409_reverse.fastq --careful -t 7 -o ./TW16409/assembly
+
+spades.py -1 TW16420/TW16420_forward.fastq -2 TW16420/TW16420_reverse.fastq --careful -t 7 -o ./TW16420/assembly
+
+spades.py -1 TW16431/TW16431_forward.fastq -2 TW16431/TW16431_reverse.fastq --careful -t 7 -o ./TW16431/assembly
+
+spades.py -1 TW16438/TW16438_forward.fastq -2 TW16438/TW16438_reverse.fastq --careful -t 7 -o ./TW16438/assembly
+
+spades.py -1 TW16442/TW16442_forward.fastq -2 TW16442/TW16442_reverse.fastq --careful -t 7 -o ./TW16442/assembly
+
+spades.py -1 TW16443/TW16443_forward.fastq -2 TW16443/TW16443_reverse.fastq --careful -t 7 -o ./TW16443/assembly
+
+spades.py -1 TW16444/TW16444_forward.fastq -2 TW16444/TW16444_reverse.fastq --careful -t 7 -o ./TW16444/assembly
+
+spades.py -1 TW16445/TW16445_forward.fastq -2 TW16445/TW16445_reverse.fastq --careful -t 7 -o ./TW16445/assembly
+
+spades.py -1 TW16452/TW16452_forward.fastq -2 TW16452/TW16452_reverse.fastq --careful -t 7 -o ./TW16452/assembly
+
+spades.py -1 TW16455/TW16455_forward.fastq -2 TW16455/TW16455_reverse.fastq --careful -t 7 -o ./TW16455/assembly
+
+spades.py -1 TW16471/TW16471_forward.fastq -2 TW16471/TW16471_reverse.fastq --careful -t 7 -o ./TW16471/assembly
+
+spades.py -1 TW16475/TW16475_forward.fastq -2 TW16475/TW16475_reverse.fastq --careful -t 7 -o ./TW16475/assembly
+
+spades.py -1 TW16475_original/TW16475_original_forward.fastq -2 TW16475_original/TW16475_original_reverse.fastq --careful -t 7 -o ./TW16475_original/assembly
+
+spades.py -1 TW16495/TW16495_forward.fastq -2 TW16495/TW16495_reverse.fastq --careful -t 7 -o ./TW16495/assembly
+
+spades.py -1 TW16498_ahem/TW16498_ahem_forward.fastq -2 TW16498_ahem/TW16498_ahem_reverse.fastq--careful -t 7 -o ./TW16498_ahem/assembly
+
+spades.py -1 TW16512/TW16512_forward.fastq -2 TW16512/TW16512_reverse.fastq --careful -t 7 -o ./TW16512/assembly
+
+spades.py -1 TW16649/TW16649_forward.fastq -2 TW16649/TW16649_reverse.fastq --careful -t 7 -o ./TW16649/assembly
+
+spades.py -1 TW16698/TW16698_forward.fastq -2 TW16698/TW16698_reverse.fastq --careful -t 7 -o ./TW16698/assembly
+
+spades.py -1 US0991/US0991_forward.fastq -2 US0991/US0991_reverse.fastq --careful -t 7 -o ./US0991/assembly
+
+spades.py -1 US0992/US0992_forward.fastq -2 US0992/US0992_reverse.fastq --careful -t 7 -o ./US0992/assembly
+
+spades.py -1 US0994/US0994_forward.fastq -2 US0994/US0994_reverse.fastq --careful -t 7 -o ./US0994/assembly
+
+spades.py -1 US0996/US0996_forward.fastq -2 US0996/US0996_reverse.fastq --careful -t 7 -o ./US0996/assembly
+
+spades.py -1 US0997/US0997_forward.fastq -2 US0997/US0997_reverse.fastq --careful -t 7 -o ./US0997/assembly
+
+spades.py -1 US0999/US0999_forward.fastq -2 US0999/US0999_reverse.fastq --careful -t 7 -o ./US0999/assembly
+
+spades.py -1 US1001/US1001_forward.fastq -2 US1001/US1001_reverse.fastq --careful -t 7 -o ./US1001/assembly
+
+spades.py -1 US1003/US1003_forward.fastq -2 US1003/US1003_reverse.fastq --careful -t 7 -o ./US1003/assembly
+
+spades.py -1 US1005/US1005_forward.fastq -2 US1005/US1005_reverse.fastq --careful -t 7 -o ./US1005/assembly
+
+spades.py -1 US1006/US1006_forward.fastq -2 US1006/US1006_reverse.fastq --careful -t 7 -o ./US1006/assembly
+
+spades.py -1 US1008/US1008_forward.fastq -2 US1008/US1008_reverse.fastq --careful -t 7 -o ./US1008/assembly
+
+spades.py -1 US1011/US1011_forward.fastq -2 US1011/US1011_reverse.fastq --careful -t 7 -o ./US1011/assembly
+
+spades.py -1 US1015/US1015_forward.fastq -2 US1015/US1015_reverse.fastq --careful -t 7 -o ./US1015/assembly
+
+spades.py -1 US1016/US1016_forward.fastq -2 US1016/US1016_reverse.fastq --careful -t 7 -o ./US1016/assembly
+
+spades.py -1 US1025/US1025_forward.fastq -2 US1025/US1025_reverse.fastq --careful -t 7 -o ./US1025/assembly
+
+spades.py -1 US1028/US1028_forward.fastq -2 US1028/US1028_reverse.fastq --careful -t 7 -o ./US1028/assembly
+
+spades.py -1 US1034/US1034_forward.fastq -2 US1034/US1034_reverse.fastq --careful -t 7 -o ./US1034/assembly
+
+spades.py -1 US1036/US1036_forward.fastq -2 US1036/US1036_reverse.fastq --careful -t 7 -o ./US1036/assembly
+
+spades.py -1 US1038/US1038_forward.fastq -2 US1038/US1038_reverse.fastq --careful -t 7 -o ./US1038/assembly
+
+spades.py -1 US1039/US1039_forward.fastq -2 US1039/US1039_reverse.fastq --careful -t 7 -o ./US1039/assembly
+
+spades.py -1 US1040/US1040_forward.fastq -2 US1040/US1040_reverse.fastq --careful -t 7 -o ./US1040/assembly
+
+spades.py -1 US1044/US1044_forward.fastq -2 US1044/US1044_reverse.fastq --careful -t 7 -o ./US1044/assembly
+
+spades.py -1 US1048/US1048_forward.fastq -2 US1048/US1048_reverse.fastq --careful -t 7 -o ./US1048/assembly
+
+spades.py -1 US1050/US1050_forward.fastq -2 US1050/US1050_reverse.fastq --careful -t 7 -o ./US1050/assembly
+
+spades.py -1 US1055/US1055_forward.fastq -2 US1055/US1055_reverse.fastq --careful -t 7 -o ./US1055/assembly
+
+spades.py -1 US1058/US1058_forward.fastq -2 US1058/US1058_reverse.fastq --careful -t 7 -o ./US1058/assembly
+
+spades.py -1 US1059/US1059_forward.fastq -2 US1059/US1059_reverse.fastq --careful -t 7 -o ./US1059/assembly
+
+spades.py -1 US1060/US1060_forward.fastq -2 US1060/US1060_reverse.fastq --careful -t 7 -o ./US1060/assembly
+
+spades.py -1 US1061/US1061_forward.fastq -2 US1061/US1061_reverse.fastq --careful -t 7 -o ./US1061/assembly
+
+spades.py -1 US1064/US1064_forward.fastq -2 US1064/US1064_reverse.fastq --careful -t 7 -o ./US1064/assembly
+
+spades.py -1 US1065/US1065_forward.fastq -2 US1065/US1065_reverse.fastq --careful -t 7 -o ./US1065/assembly
+
+spades.py -1 US1067/US1067_forward.fastq -2 US1067/US1067_reverse.fastq --careful -t 7 -o ./US1067/assembly
+
+spades.py -1 US1069/US1069_forward.fastq -2 US1069/US1069_reverse.fastq --careful -t 7 -o ./US1069/assembly
+
+spades.py -1 US1070/US1070_forward.fastq -2 US1070/US1070_reverse.fastq --careful -t 7 -o ./US1070/assembly
+
+spades.py -1 US1071/US1071_forward.fastq -2 US1071/US1071_reverse.fastq --careful -t 7 -o ./US1071/assembly
+
+spades.py -1 US1076/US1076_forward.fastq -2 US1076/US1076_reverse.fastq --careful -t 7 -o ./US1076/assembly
+
+spades.py -1 US1078/US1078_forward.fastq -2 US1078/US1078_reverse.fastq --careful -t 7 -o ./US1078/assembly
+
+spades.py -1 US1081/US1081_forward.fastq -2 US1081/US1081_reverse.fastq --careful -t 7 -o ./US1081/assembly
+
+spades.py -1 US1084/US1084_forward.fastq -2 US1084/US1084_reverse.fastq --careful -t 7 -o ./US1084/assembly
+
+spades.py -1 US1085/US1085_forward.fastq -2 US1085/US1085_reverse.fastq --careful -t 7 -o ./US1085/assembly
+
+spades.py -1 US1086/US1086_forward.fastq -2 US1086/US1086_reverse.fastq --careful -t 7 -o ./US1086/assembly
+
+spades.py -1 US1087/US1087_forward.fastq -2 US1087/US1087_reverse.fastq --careful -t 7 -o ./US1087/assembly
+
+spades.py -1 US1088/US1088_forward.fastq -2 US1088/US1088_reverse.fastq --careful -t 7 -o ./US1088/assembly
+
+spades.py -1 US1096/US1096_forward.fastq -2 US1096/US1096_reverse.fastq --careful -t 7 -o ./US1096/assembly
+
+spades.py -1 US1100/US1100_forward.fastq -2 US1100/US1100_reverse.fastq --careful -t 7 -o ./US1100/assembly
+
+spades.py -1 US1105/US1105_forward.fastq -2 US1105/US1105_reverse.fastq --careful -t 7 -o ./US1105/assembly
+
+spades.py -1 US1109/US1109_forward.fastq -2 US1109/US1109_reverse.fastq --careful -t 7 -o ./US1109/assembly
+
+spades.py -1 US1112/US1112_forward.fastq -2 US1112/US1112_reverse.fastq --careful -t 7 -o ./US1112/assembly
+
+spades.py -1 US1113/US1113_forward.fastq -2 US1113/US1113_reverse.fastq --careful -t 7 -o ./US1113/assembly
+
+spades.py -1 US1114/US1114_forward.fastq -2 US1114/US1114_reverse.fastq --careful -t 7 -o ./US1114/assembly
+
+spades.py -1 US1118/US1118_forward.fastq -2 US1118/US1118_reverse.fastq --careful -t 7 -o ./US1118/assembly
+
+spades.py -1 US1119/US1119_forward.fastq -2 US1119/US1119_reverse.fastq --careful -t 7 -o ./US1119/assembly
+
+spades.py -1 US1120/US1120_forward.fastq -2 US1120/US1120_reverse.fastq --careful -t 7 -o ./US1120/assembly
+
+spades.py -1 US1121/US1121_forward.fastq -2 US1121/US1121_reverse.fastq --careful -t 7 -o ./US1121/assembly
+
+spades.py -1 US1124/US1124_forward.fastq -2 US1124/US1124_reverse.fastq --careful -t 7 -o ./US1124/assembly
+
+spades.py -1 US1125/US1125_forward.fastq -2 US1125/US1125_reverse.fastq --careful -t 7 -o ./US1125/assembly
+
+spades.py -1 US1126/US1126_forward.fastq -2 US1126/US1126_reverse.fastq --careful -t 7 -o ./US1126/assembly
+
+spades.py -1 US1129/US1129_forward.fastq -2 US1129/US1129_reverse.fastq --careful -t 7 -o ./US1129/assembly
+
+spades.py -1 US1131/US1131_forward.fastq -2 US1131/US1131_reverse.fastq --careful -t 7 -o ./US1131/assembly
+
+spades.py -1 US1132/US1132_forward.fastq -2 US1132/US1132_reverse.fastq --careful -t 7 -o ./US1132/assembly
+
+spades.py -1 US1133/US1133_forward.fastq -2 US1133/US1133_reverse.fastq --careful -t 7 -o ./US1133/assembly
+
+spades.py -1 US1134/US1134_forward.fastq -2 US1134/US1134_reverse.fastq --careful -t 7 -o ./US1134/assembly
+
+spades.py -1 US1135/US1135_forward.fastq -2 US1135/US1135_reverse.fastq --careful -t 7 -o ./US1135/assembly
+
+# end
