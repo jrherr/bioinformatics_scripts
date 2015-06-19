3ef8b02de51a3bc9550d0a7f374c84831d18f601
Josh Herr <joshua.r.herr@gmail.com>
2015-06-19 09:22:11 -0400

    updated perl scripts

---
+++ perl_scripts/remove_ambiguous.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+
+###############################################################################
+# Title:remove_ambiguous.pl
+# Purpose:removes ambiguity codes from fasta files\
+# Usage: perl remove_ambiguous.pl input.fasta output.fasta
+# scripts takes compressed files
+###############################################################################
+
+
+use warnings;
+use strict;
+
+###############################################################################
+# Code to handle help menu and man page
+###############################################################################
+use Getopt::Long;
+use Pod::Usage;
+my $man  = 0;
+my $help = 0;
+my $toN;
+GetOptions('N' => \$toN, 'help|?' => \$help, man => \$man) or pod2usage(2);
+pod2usage(2) if $help;
+pod2usage(-verbose => 2) if $man;
+pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
+
+###############################################################################
+# Automatically extract compressed files
+###############################################################################
+@ARGV = map { s/(.*\.gz)\s*$/pigz -dc < $1|/; s/(.*\.bz2)\s*$/pbzip2 -dc < $1|/; $_ } @ARGV;
+###############################################################################
+# remove_ambiguous.pl
+###############################################################################
+
+use ReadFastx;
+
+my %mapping = ('[MRWVHDN]' => 'A', '[YKB]' => 'T', S => 'C');
+
+%mapping = ('[MRWVHDYKBS]' => 'N') if $toN;
+
+my $file = ReadFastx->new();
+
+while (my $seq = $file->next_seq) {
+  for my $map (keys %mapping) {
+    my $sequence = $seq->sequence;
+    $sequence =~ s/$map/$mapping{$map}/gi;
+    $seq->sequence($sequence);
+  }
+  $seq->print(width => 80);
+}
+
+###############################################################################
+# Help Documentation
+###############################################################################
+__END__
+
+=head1 NAME
+
+remove_ambiguous.pl - removes ambiguity codes from fasta files
+
+=head1 SYNOPSIS
+
+remove_ambiguous.pl [options] [file ...]
+
+Options:
+      -help
+      -man               for more info
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-N>
+
+convert each ambiguous mapping to an N
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+B<remove_ambiguous.pl> removes ambiguity codes from fasta files
+
+=cut
